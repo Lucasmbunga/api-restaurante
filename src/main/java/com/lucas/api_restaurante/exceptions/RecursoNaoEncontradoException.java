dfa7e1860a43c8374dd7774cacc6df5edd600d7f
Lucas Mbunga António Pascoal <bernivaldolucaspascoal@gmail.com>
2026-03-21 19:33:43 +0100

    Tratamento global de mais algumas exceções personalizadas

--- src/main/java/com/lucas/api_restaurante/exceptions/RecursoNaoEncontradoException.java
+++
@@ -1,9 +0,0 @@
-package com.lucas.api_restaurante.exceptions;
-
-
-public class RecursoNaoEncontradoException extends Exception {
-
-    public RecursoNaoEncontradoException(String message) {
-        super(message);
-    }
-}
